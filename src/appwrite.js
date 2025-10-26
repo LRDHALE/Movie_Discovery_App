c64db412b80ec967b436a91fa87031af102ac2c5
LRDhale <likhitrajendradhale@gmail.com>
2025-10-26 09:31:23 +0530

    feat: appwrite.js updated

--- src/appwrite.js
+++ src/appwrite.js
@@ -5,13 +5,13 @@ const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
 const client = new Client()
-  .setEndpoint('https://fra.cloud.appwrite.io/v1')
+  .setEndpoint('https://cloud.appwrite.io/v1')
   .setProject(PROJECT_ID)
 
 const database = new Databases(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
-  // 1. Use Appwrite SDK to check if the search term exists in the database
+
  try {
   const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
     Query.equal('searchTerm', searchTerm),
